4eef56112f77140a5501a8a9620ad9008859a716
David Mediati <davidmediati@gmail.com>
2019-01-17 23:55:06 -0500

    removed style from html

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,18 +1,22 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+document.body.style.margin = "0px";
+document.body.style.overflow = "hidden";
+let canvas = document.createElement("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+document.body.appendChild(canvas);
+
+let ctx = canvas.getContext("2d");
 
 export default function draw(offset) {
-    ctx.canvas.width  = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
     ctx.beginPath();
     ctx.rect(0, 0, window.innerWidth/2, window.innerHeight);
     ctx.fillStyle = '#ff0000';
     ctx.fill();
-    ctx.closePath()
+    ctx.closePath();
 
     ctx.beginPath();
     ctx.rect(window.innerWidth/2, 0, window.innerWidth/2, window.innerHeight);
     ctx.fillStyle = '#000099';
     ctx.fill();
-    ctx.closePath()
+    ctx.closePath();
 }
